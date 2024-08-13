7444374470b5eaf70fb4d721cf18fa33a406c188
Ferrel John Fernando <38653000+synt4xer@users.noreply.github.com>
2024-08-13 07:42:41 +0700

    ref: logger and README.md (#5)

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -9,7 +9,8 @@ import (
 )
 
 func ProvideLog() {
-	logLevel := os.Getenv("LOG_LEVEL")
+	logLevel := os.Getenv("APP_ENV")
+	isDebug := os.Getenv("APP_DEBUG") == "true"
 
 	var logger zerolog.Logger
 
@@ -21,12 +22,17 @@ func ProvideLog() {
 		zerolog.TimeFieldFormat = zerolog.TimeFormatUnix
 		zerolog.ErrorStackMarshaler = pkgerrors.MarshalStack
 		logger = zerolog.New(os.Stdout).With().Timestamp().Logger()
-	case "development":
+	case "development", "local":
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		logger = zerolog.New(output).With().Timestamp().Caller().Logger()
+
+		if isDebug {
+			logger = zerolog.New(output).With().Timestamp().Caller().Logger()
+		} else {
+			logger = zerolog.New(output).With().Timestamp().Logger()
+		}
 	default:
 		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-		logger = zerolog.New(output).With().Timestamp().Caller().Logger()
+		logger = zerolog.New(output).With().Timestamp().Logger()
 	}
 
 	log.Logger = logger
