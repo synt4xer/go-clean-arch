2e3b3dd01ceac4514d9a0be94e1801788ecf4cf9
ferrel john fernando <f.syntaxer@gmail.com>
2024-08-21 00:38:24 +0700

    ref: database drivers and migration

--- database/migrations/20240812182211_create_users.sql
+++ database/migrations/20240812182211_create_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE `users` (
     `id` int NOT NULL AUTO_INCREMENT,
     `email` varchar(255) NOT NULL,
     `password` text NOT NULL,
-    `fullname` varchar(255) NOT NULL,
+    `full_name` varchar(255) NOT NULL,
     `phone_number` varchar(15) NOT NULL,
     `is_active` tinyint(1) NOT NULL,
     `created_at` timestamp NOT NULL,
