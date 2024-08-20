4a55fbdd1c6fa2d020e33ebbaa8ad15fcd3b8eed
Ferrel John Fernando <38653000+synt4xer@users.noreply.github.com>
2024-08-21 00:38:51 +0700

    ref: Database drivers and migration (#8)

--- database/drivers/mysql_sqlx.go
+++ database/drivers/mysql_sqlx.go
@@ -4,22 +4,28 @@ import (
 	"context"
 	"fmt"
 	"github.com/jmoiron/sqlx"
+	"github.com/rs/zerolog/log"
 	"github.com/synt4xer/go-clean-arch/config"
-	"time"
+	"go.uber.org/fx"
+	"os"
 )
 
-func NewDB(ctx context.Context, cfg *config.DatabaseConfig) (*sqlx.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True", cfg.Username, cfg.Password, cfg.Host, cfg.Port, cfg.Name)
+func NewDB(lc fx.Lifecycle, cfg *config.Config) *sqlx.DB {
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true",
+		cfg.Database.Username, cfg.Database.Password, cfg.Database.Host, cfg.Database.Port, cfg.Database.Name)
 
-	db, err := sqlx.ConnectContext(ctx, "mysql", dsn)
-
+	db, err := sqlx.ConnectContext(context.Background(), "mysql", dsn)
 	if err != nil {
-		return nil, err
+		log.Error().Err(err).Msg("cannot connect to database")
+		os.Exit(1)
 	}
 
-	db.SetConnMaxLifetime(5 * time.Minute)
-	db.SetMaxIdleConns(5)
-	db.SetMaxOpenConns(95)
+	lc.Append(fx.Hook{
+		OnStop: func(_ context.Context) error {
+			_ = db.Close()
+			return nil
+		},
+	})
 
-	return db, nil
+	return db
 }
