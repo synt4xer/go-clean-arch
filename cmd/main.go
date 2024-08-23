984647cc2fd0980369d34cc641b0d96700219e31
Ferrel John Fernando <38653000+synt4xer@users.noreply.github.com>
2024-08-23 18:56:39 +0700

    feat: Add routes and module (#18)

--- cmd/main.go
+++ cmd/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/synt4xer/go-clean-arch/cmd/app"
 	"github.com/synt4xer/go-clean-arch/cmd/fx"
 	"github.com/synt4xer/go-clean-arch/config"
-	"github.com/synt4xer/go-clean-arch/database/drivers"
-	"net/http"
+	"github.com/synt4xer/go-clean-arch/database"
+	"github.com/synt4xer/go-clean-arch/internal/module"
 )
 
 func main() {
 	cfg, _ := config.ProvideConfig()
 
 	apps := app.New(cfg)
-	apps.AddProviders(config.ProvideConfig, drivers.NewDB, fx.AddRoutes)
-	//apps.AddModules()
+	apps.AddProviders(config.ProvideConfig, database.NewDB, fx.AddRoutes)
+	apps.AddModules(module.UsersModule)
 	apps.AddServers(fx.NewHttpServer)
 	apps.AddInvokers(func(*http.Server) {})
 
